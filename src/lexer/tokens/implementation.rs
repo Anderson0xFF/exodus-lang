2c8512bdf3b54c1dcce27f75186610c58f3818cb
Anderson Almeida <andersonvsc1998@gmail.com>
2023-02-12 23:17:56 -0300

    add variable

--- src/lexer/tokens/implementation.rs
+++ src/lexer/tokens/implementation.rs
@@ -1,26 +1,119 @@
 #![allow(dead_code)]
 
-use super::{Token, Operator};
+use crate::lexer::tokens::{Unary, Value};
+
+use super::{Expression, Let, Operator, Token, Type};
 use logos::Lexer;
 
-impl Token {
-    pub fn variable(lex: &mut Lexer<Token>) -> Vec<Token> {
-        let mut body = Vec::new();
+impl Let {
+    pub fn default(lex: &mut Lexer<Token>) -> Let {
+        let name = match Token::next_valided_token(lex) {
+            Token::Identifier(name) => name,
+            token => Token::expected_panic(token, Token::Identifier("'varname'".to_owned())),
+        };
+
+        Token::expected(lex, Token::Assing);
+        let mut expr = Vec::new();
+        let mut operator: Option<Operator> = None;
+
         loop {
-            match lex.next() {
-                Some(v) => match v {
-                    Token::Space => (),
-                    Token::Assing => (),
-                    Token::IntValue(_) => body.push(v),
-                    Token::Semicolon => break,
-                    Token::Operator(op) => body.push(Token::Operator(op)),
-                    Token::Identifier(_) => body.push(v),
-                    _ => break,
-                },
-                _ => break,
+            match Token::next_valided_token(lex) {
+                Token::String(value) => expr.push(Expression::Unary(Unary {
+                    operator: operator.clone(),
+                    value: Value::STRING(value),
+                })),
+                Token::IntValue(value) => expr.push(Expression::Unary(Unary {
+                    operator: operator.clone(),
+                    value: Value::I32(value as i32),
+                })),
+                Token::Operator(op) => {
+                    operator = Some(op);
+                }
+                Token::Identifier(value) => expr.push(Expression::Unary(Unary {
+                    operator: operator.clone(),
+                    value: Value::REF(value),
+                })),
+                Token::Semicolon => break,
+                _ => (),
+            }
+        }
+
+        let expr = Expression::bind_expression(expr);
+
+        Self {
+            name,
+            typed: None,
+            expr,
+        }
+    }
+}
+
+impl Type {
+    pub fn default(lex: &mut Lexer<Token>) -> Type {
+        match lex.slice() {
+            "i8" => Type::I8,
+            "i16" => Type::I16,
+            "i32" => Type::I32,
+            "i64" => Type::I64,
+            "u8" => Type::U8,
+            "u16" => Type::U16,
+            "u32" => Type::U32,
+            "u64" => Type::U64,
+            "f32" => Type::F32,
+            "f64" => Type::F64,
+            "char" => Type::Char,
+            "string" => Type::String,
+            "bool" => Type::Boolean,
+            _ => todo!(),
+        }
+    }
+}
+
+impl Token {
+    pub fn expr(lex: &mut Lexer<Token>) -> Expression {
+        println!("{:?}", lex);
+        todo!()
+    }
+
+    pub fn if_conditional(lex: &mut Lexer<Token>) -> Vec<Token> {
+        let mut body = Vec::new();
+        Self::expected(lex, Token::LP);
+        loop {
+            let token = Self::next_valided_token(lex);
+            match token {
+                Token::Identifier(_) => body.push(token),
+                //Token::String(_) => body.push(token),
+                Token::RP => break,
+                //Token::IntValue(_) => body.push(token),
+                Token::Operator(_) => body.push(token),
+                _ => Self::expected_panic(token, Token::RP),
             }
         }
         return body;
+    }
+
+    fn expected(lex: &mut Lexer<Token>, expected: Token) {
+        let current = Self::next_valided_token(lex);
+        if current != expected {
+            Self::expected_panic(current, expected);
+        }
+    }
+
+    fn expected_panic(current: Token, expected: Token) -> ! {
+        panic!("expected '{:?}' got '{:?}'", expected, current);
+    }
+
+    fn next_valided_token(lex: &mut Lexer<Token>) -> Token {
+        loop {
+            if let Some(item) = lex.next() {
+                match item {
+                    Token::Space => continue,
+                    Token::LINEBREAK => continue,
+                    Token::Error => panic!("Error token"),
+                    _ => break item,
+                }
+            }
+        }
     }
 
     pub fn operator(lex: &mut Lexer<Token>) -> Operator {
@@ -30,15 +123,22 @@ impl Token {
             "*" => Operator::MUL,
             "/" => Operator::DIV,
             "%" => Operator::MOD,
-            _=> todo!()
+            "==" => Operator::EQUAL,
+            "<" => Operator::LT,
+            ">" => Operator::GT,
+            "!=" => Operator::NOTEQ,
+            "&&" => Operator::AND,
+            "||" => Operator::OR,
+            "&" => Operator::REF,
+            "!" => Operator::NOT,
+            _ => todo!(),
         }
     }
 
-    pub fn get_operator(&self) -> Option<Operator>{
+    pub fn get_operator(&self) -> Option<Operator> {
         match self {
-            Token::Operator(token) =>  Some(token.clone()),
+            Token::Operator(token) => Some(token.clone()),
             _ => None,
         }
     }
-
 }
--- src/lexer/tokens/mod.rs
+++ src/lexer/tokens/mod.rs
@@ -1,6 +1,106 @@
 use logos::Logos;
 pub mod implementation;
 
+#[derive(Debug, PartialEq, Clone)]
+pub enum Type {
+    I8,
+    I16,
+    I32,
+    I64,
+    U8,
+    U16,
+    U32,
+    U64,
+    F32,
+    F64,
+    Boolean,
+    Char,
+    String,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct Let {
+    name: String,
+    typed: Option<Type>,
+    expr: Vec<Expression>,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct Unary {
+    operator: Option<Operator>,
+    value: Value,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct Binary {
+    x: Unary,
+    operator: Operator,
+    y: Unary,
+}
+
+impl Binary {
+    pub fn new(x: Unary, operator: Operator, y: Unary) -> Self {
+        Self { x, operator, y }
+    }
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum Expression {
+    Unary(Unary),
+    Binary(Binary),
+}
+
+impl Expression {
+    pub fn bind_expression(exprs: Vec<Expression>) -> Vec<Expression> {
+        if exprs.len() == 1 {
+            return exprs;
+        }
+
+        let mut expr = Vec::new();
+        let mut intter = exprs.iter();
+
+        loop {
+            let x = match intter.next() {
+                Some(exp) => match exp {
+                    Expression::Unary(u) => u.clone(),
+                    _=> panic!()
+                },
+                None => break,
+            };
+            let y = match intter.next() {
+                Some(exp) => match exp {
+                    Expression::Unary(u) => u.clone(),
+                    _=> panic!()
+                },
+                None => {
+                    expr.push(Expression::Unary(x));
+                    break;
+                }
+            };
+
+            let operator = y.clone().operator.unwrap();
+
+            let binary = Binary::new(x, operator, y);
+            expr.push(Expression::Binary(binary));
+        }
+        return expr;
+    }
+}
+
+#[derive(Debug, PartialEq, Clone)]
+enum Value {
+    CHAR(char),
+    STRING(String),
+    I8(i8),
+    I16(i16),
+    I32(i32),
+    I64(i64),
+    U8(u8),
+    U16(u16),
+    U32(u32),
+    U64(u64),
+    REF(String),
+}
 
 #[derive(Logos, Debug, PartialEq, Clone)]
 pub enum Token {
@@ -10,10 +110,16 @@ pub enum Token {
     #[regex(r#""[^"]*""#, |lexer| lexer.slice()[1..(lexer.slice().len()-1)].to_owned())]
     String(String),
 
-    #[token("let" , Token::variable)]
-    Let(Vec<Token>),
+    #[token("let", Let::default)]
+    Let(Let),
 
-    #[token("if" , Token::variable)]
+    #[regex(
+        "(i8|i16|i32|i64|u8|u16|u32|u64|f32|f64|string|char|bool)",
+        Type::default
+    )]
+    Type(Type),
+
+    #[token("if", Token::if_conditional)]
     If(Vec<Token>),
 
     #[token(" ")]
@@ -28,10 +134,13 @@ pub enum Token {
     Semicolon,
     #[token("=")]
     Assing,
+
     #[token("(")]
     LP,
+
     #[token(")")]
     RP,
+
     #[token("{")]
     LB,
     #[token("}")]
@@ -45,13 +154,23 @@ pub enum Token {
 
     #[error]
     Error,
+    #[token("\n")]
+    LINEBREAK,
 }
 
 #[derive(Debug, PartialEq, Clone)]
 pub enum Operator {
-    ADD, 
+    ADD,
     SUB,
     MUL,
     DIV,
     MOD,
+    EQUAL,
+    LT,
+    GT,
+    AND,
+    NOTEQ,
+    OR,
+    REF,
+    NOT,
 }
