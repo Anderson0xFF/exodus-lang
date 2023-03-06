be0b4208b02a2ca23e41da53590314f231bd722b
Anderson Almeida <andersonvsc1998@gmail.com>
2023-03-06 19:53:58 -0300

    add get_source_line and refactory

--- exodusc_lexer/src/lib.rs
+++ exodusc_lexer/src/lib.rs
@@ -1,5 +1,5 @@
-use scanner::Token;
 use logos::Logos;
+use scanner::Token;
 
 pub mod scanner;
 pub mod types;
@@ -9,8 +9,10 @@ mod tests;
 
 pub struct Lexer {
     filename: String,
+    source: String,
     data: Vec<Token>,
     line: usize,
+    cursor: usize,
     idx: i32,
 }
 
@@ -22,9 +24,11 @@ impl Lexer {
             buff.push(token);
         }
         Self {
-            data: buff,
             filename: filename.to_string(),
+            source: source.to_string(),
+            data: buff,
             line: 1,
+            cursor: 0,
             idx: 0,
         }
     }
@@ -35,24 +39,25 @@ impl Lexer {
     }
 
     pub fn next(&mut self) -> Token {
-        loop {
-            if self.idx >= self.data.len() as i32 {
-                return Token::EOF;
-            }
-
-            match self.data[self.idx as usize].clone() {
+        while self.idx < self.data.len() as i32 {
+            let token = &self.data[self.idx as usize];
+            self.idx += 1;
+            self.cursor += token.to_string().len();
+            match token {
                 Token::Line => {
                     self.line += 1;
-                    self.idx += 1;
+                    self.cursor = 0;
+                    continue;
                 }
-                Token::Space => self.idx += 1,
-                Token::Error => self.idx += 1,
-                token => {
-                    self.idx += 1;
-                    return token;
-                }
+                Token::Space | Token::Error => continue,
+                _ => return token.clone(),
             }
         }
+        Token::EOF
+    }
+
+    pub fn column(&self) -> usize {
+        self.cursor
     }
 
     pub fn peek(&mut self) -> Token {
@@ -68,22 +73,37 @@ impl Lexer {
         self.idx += 1;
     }
 
-    pub fn back(&mut self) -> Token {
-        loop {
-            if self.idx - 1 <= 0 {
-                return Token::EOF;
-            }
+    pub fn skip_line(&mut self) {
+        let line = self.line;
+        while self.line == line {
+            self.next();
+        }
+        self.back();
+    }
 
+    pub fn next_identifier(&mut self) -> Option<String> {
+        match self.next() {
+            scanner::Token::Identifier(name) => Some(name.to_owned()),
+            _ => None,
+        }
+    }
+
+    pub fn back(&mut self) {
+        while self.idx - 1 > 0 {
             match self.data[(self.idx - 1) as usize].clone() {
                 Token::Line => {
                     self.line -= 1;
                     self.idx -= 1;
-                },
-                Token::Space => self.idx -= 1,
-                Token::Error => self.idx -= 1,
-                tk => {
+                    self.cursor = 0;
+                }
+                Token::Space | Token::Error => {
                     self.idx -= 1;
-                    return tk;
+                    self.cursor -= 1
+                }
+                token => {
+                    self.cursor -= token.to_string().len();
+                    self.idx -= 1;
+                    return;
                 }
             }
         }
@@ -95,5 +115,34 @@ impl Lexer {
 
     pub fn line(&self) -> usize {
         self.line
+    }
+
+    pub fn get_source_line(&self, line_number: usize) -> Option<&str> {
+        let mut current_line = 1;
+        let mut start_idx = 0;
+        let mut end_idx = 0;
+        for (i, c) in self.source.char_indices() {
+            if current_line == line_number && (i == 0 || &self.source[i - 1..i] == "\n") {
+                start_idx = i;
+            }
+            if current_line == line_number && c == '\n' {
+                end_idx = i;
+                break;
+            }
+            if c == '\n' {
+                current_line += 1;
+            }
+        }
+        if end_idx == 0 && current_line == line_number {
+            end_idx = self.source.len();
+        }
+        if start_idx == 0 && end_idx == 0 {
+            return None;
+        }
+        Some(&self.source[start_idx..end_idx])
+    }
+
+    pub fn source(&self) -> &str {
+        self.source.as_ref()
     }
 }
